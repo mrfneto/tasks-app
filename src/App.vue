fc8465427f9600dfbf49c0e1dee1740eb18101e4
mrfneto <mrfneto@gmail.com>
2025-04-04 20:48:03 -0300

    chore: initialize project with Vue 3 and Tailwind CSS 4

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// imports
+
+//props e emits
+
+// states
+
+// Computed properties
+
+// Métodos / funções
+
+</script>
+<template>
+<div>
+
+</div>
+</template>
+
+<style scoped>
+/* Apenas estilos específicos do componente */
+</style>
